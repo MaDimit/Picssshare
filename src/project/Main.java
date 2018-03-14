24264368ce4068093f1ab75d2eb6dfbdf9dc9f96
[Philip] <[amatrixable@gmail.com]>
2018-03-14 20:13:20 +0200

    method in Main.java

--- src/project/Main.java
+++ src/project/Main.java
@@ -3,4 +3,8 @@ package project;
 public class Main {
 	//neshto drugo sled nego, da sme na edin red
 	//My comment ggggggggggggggggggggggggg
+	
+	public static void methoddd() {
+		System.out.println("ASDASDASDASDAS");
+	}
 }
