b901811c9e035942075b6291b0975d7852836a85
[Philip] <amatrixable@gmail.com>
2018-04-12 15:42:52 +0300

    search box added

---
+++ src/controller/servlets/SearchUsersServlet.java
@@ -0,0 +1,31 @@
+package controller.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.manager.CollectionsManager;
+import model.UserBean;
+
+/**
+ * Servlet implementation class SearchUsersServlet
+ */
+@WebServlet("/searchUsers")
+public class SearchUsersServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<UserBean> foundUsers = (ArrayList<UserBean>) CollectionsManager.getInstance().searchUser(request.getParameter("username"));
+		for(int i = 0 ; i<foundUsers.size();i++) {
+			response.getWriter().println(foundUsers.get(i).getUsername());
+		}
+		request.setAttribute("foundUsers", foundUsers);
+		request.getRequestDispatcher("search.jsp").forward(request, response);
+	}
+
+}
